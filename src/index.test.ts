677240668ce87e10b240a8c33c5ebb11e3ff83ac
l1b3r_- <liber@ricardojrm.com>
2021-12-02 21:59:13 +0000

    fix: use 'it' instead of 'test'

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { novaHelloWorld } from './index';
 
-test('console.log "Hello World from Nova"', () => {
+it('should console.log "Hello World from Nova"', () => {
   console.log = jest.fn();
   novaHelloWorld();
   expect(console.log).toHaveBeenCalledWith('Hello World from Nova');
