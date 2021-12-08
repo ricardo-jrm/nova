68f347b5be5f128415494390fd6a1a2af19cdfdf
l1b3r_- <liber@ricardojrm.com>
2021-12-08 01:48:02 +0000

    feat: add ts-jest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,12 @@
 import { novaHelloWorld } from '.';
 
+const sum = (a: number, b: number) => a + b;
+
+it('sums 5 and 2 returns 7', () => {
+  const a: number = 5;
+  expect(sum(5, 2)).toBe(7);
+});
+
 describe('example test suite', () => {
   it('should console.log "Hello World from Nova"', () => {
     console.log = jest.fn(); // eslint-disable-line no-console
